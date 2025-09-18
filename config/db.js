1c00c4cf55e4dd567b9f163fd85d823997fc0be2
Souvik Roy <roysouvik500@gmail.com>
2025-09-18 13:27:01 +0530

    new change

--- config/db.js
+++ config/db.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected");
+    console.log("MongoDB Connected");
   } catch (err) {
-    console.error("❌ MongoDB connection failed", err.message);
-    process.exit(1);
+    console.error("MongoDB connection failed", err.message);
   }
 };
 
--- server.js
+++ server.js
@@ -27,5 +27,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
